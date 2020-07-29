9095d24c0422b48d5170307eaa352e401713453c
SaiGuy1 <snarendran0304@gmail.com>
2020-07-29 14:22:30 -0700

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1> Kasa </h1>
+      <p> this is my app for Kasa interview </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
